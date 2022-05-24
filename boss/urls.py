a8bba6f269742f933eb3bb8c86df99d8ebe1e7fd
leechanghan <sjaqj88@gmail.com>
2022-05-24 13:28:27 +0900

    last commit

--- boss/urls.py
+++ boss/urls.py
@@ -4,6 +4,4 @@ from boss import views
 urlpatterns = [
     path('orders/<int:shop>', views.order_list, name="order_list"),
     path('timeinput/', views.time_input, name="timeinput"),
-    # path('menus/<int:shop>', views.menu, name="menu"),
-    # path('order/', views.order, name="order")
 ]
